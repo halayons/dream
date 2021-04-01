1c9462f6836f05dc7a5673f1c2c3814188cce19b
halayons <halayons@unal.edu.co>
2021-04-01 17:07:35 -0500

    CAKE-20 iniciar secion version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
+
 import './App.css';
+
+function Ingreso(){
+  return(<div id="ingreso">
+    <form>
+        <h1>Ingreso</h1>
+        <p>Usuario:</p>
+        <input id="caja" type="text"/>
+        <p>Contraseña:</p>
+        <input id="caja"
+          type="text"
+        />
+        <p><button id="boton" onclick="ingresar">Ingresar</button></p>
+      </form>
+  </div>)
+}
+
+function Imagen(){
+  return(<div id="imagen">
+    <img src="descaga(2).png'" />
+
+  </div>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div ><Ingreso/><Imagen/></div>
   );
 }
 
