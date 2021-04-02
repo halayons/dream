fe9bb8a3df2c15cb209394d8a7efb124bd5f81a0
halayons <halayons@unal.edu.co>
2021-04-02 14:02:37 -0500

    CAKE-20 iniciar sesion v2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import logo from './logo.svg';
+import imgone from './logo.jpg';
 
 import './App.css';
 
 function Ingreso(){
   return(<div id="ingreso">
     <form>
-        <h1>Ingreso</h1>
-        <p>Usuario:</p>
-        <input id="caja" type="text"/>
-        <p>Contraseña:</p>
-        <input id="caja"
-          type="text"
-        />
-        <p><button id="boton" onclick="ingresar">Ingresar</button></p>
+        <input id="caja" type="text" placeholder="Nombre"/>
+        <input id="caja" type="text" placeholder="Correo"/>
+        <input id="caja" type="text" placeholder="Contraseña"/>
+        <p><button id="boton1" onclick="Registrarse" >registrarse</button></p>
+        <p><button id="boton2" onclick="iniciar">Iniciar Sesion</button></p>
       </form>
   </div>)
 }
 
 function Imagen(){
   return(<div id="imagen">
-    <img src="descaga(2).png'" />
-
+    <div id= "logo" >
+    
+    </div>
+   
+    
   </div>)
 }
 
