6edd927a362fc7d99da3993d9afb8f583f2e2d25
z4yross <z4yross@gmail.com>
2021-04-07 23:58:20 -0500

    linked login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
-import {Login, Register} from "./components/login/index";
+import { Login, Register } from "./components/login/index";
 
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       isLogginActive: true,
     }
   }
-  render(){
-    const {isLogginActive}=this.state;
+
+  componentDidMount() {
+    this.renderLogin()
+  }
+
+
+  renderLogin = async () => {
+    try {
+      let res = await fetch("http://localhost:8000/accounts/login/")
+      let html = await res.text();
+
+      let parser = new DOMParser();
+      let doc = parser.parseFromString(html, "text/html");
+
+      this.setState({
+        'test': doc.body.innerHTML
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  render() {
+    const { isLogginActive } = this.state;
+
     return (
       <div className="App">
         <div className="login">
           <div className="container">
-           {!isLogginActive && <Login containerRef={(ref)=>this.current=ref}/>}
-           {isLogginActive && <Register containerRef={(ref)=>this.current=ref} />}
+            <div className="content" dangerouslySetInnerHTML={{ __html: this.state.test}}></div>
           </div>
         </div>
       </div>
     )
+
+
   }
 }
 
--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -2,23 +2,24 @@ import React from "react";
 
 export class Login extends React.Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
     }
 
+
     render() {
         return <div className="ingreso" ref={this.props.containerRef}>
-            
-            
+
+
             <div className="form">
-             <input id="caja" type="text" placeholder="Correo"/>
-        <input id="caja" type="text" placeholder="Contraseña"/>
-        <p><button id="boton1" onclick="iniciar" >Iniciar Sesion</button></p>
-        <p><button id="boton2" onClick="Registro">Nueva Cuenta</button></p>
+                <input id="caja" type="text" placeholder="Correo" />
+                <input id="caja" type="text" placeholder="Contraseña" />
+                <p><button id="boton1" onclick="iniciar" >Iniciar Sesion</button></p>
+                <p><button id="boton2" onClick="Registro">Nueva Cuenta</button></p>
 
             </div>
-          </div>
-        
+        </div>
+
     }
 
 }
