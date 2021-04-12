81b591552b9e4c286b2887380d34a8d5a0c68a2b
AndresSanchez2021 <anfsanchezcu@unal.edu.co>
2021-04-11 19:22:10 -0500

    conectado al back, lo esencial

--- src/social.js
+++ src/social.js
@@ -2,91 +2,86 @@ import './social.css';
 import imagen1 from './cake.jpg';
 import imagen2 from './cara.jpg';
 import logo from './logo.svg';
-function Social_app() {
-  return (
-   <div className ="social_app">
+import like from './like.png';
+import React from 'react';
+
+const url ="http://localhost:8000/social/";
+
+class Social_app extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            data: [],
+        };
+    }
+    funtion (a){
+        var l=a.length;
+        var ll=a.substring(13,l)
+        return ll;
+    }
+    fecha (a){
+       return a.substring(0,9);
+    }
+    usuarios(e){
+        return( <div className ="customer">
+        <div className ="cakeC">
+           <img src={"http://localhost:8000/" +this.funtion(e.foto)}  className ="cake"></img>
+        </div>
+        <div className ="photoC">
+           <img src ={imagen2} className ="photo"/>
+        </div>
+
+         <div className="infoC">
+            <td>
+             <div className ="nameC">
+                <p> usuario:</p>
+                 {e.usuario}
+             </div>
+             <div className ="date">
+                <p> fecha:</p>{this.fecha(e.published_date)}
+             </div>
+            </td>
+
+             <td>
+             <div className ="counterLikes">
+                 <img src ={like} className ="likes"></img><p>{e.likes}</p>
+             </div>
+             </td>
+        </div>
+
+
+     </div>);
+
+    }
     
-       <div className = "tittle">
-           <div><h1>Dream Cakers</h1></div>
-       </div>
-       <div className ="content">
-           <div>
-               <div className ="customer">
-                   <div className ="cakeC">
-                       <img src ={imagen1} className ="cake"/>
-                   </div>
-                   <div className ="photoC">
-                      <img src ={imagen2} className ="photo"/>
-                   </div>
+     componentWillMount=() => {
 
-                    <div className="infoC">
-                        <div className ="nameC">
-                             <p>Andres Flipe Snchez Cuatindioy</p>
-                        </div>
-                        <div className ="date">
-                            <p>6/marzo/2021</p>
-                        </div>
-                        <div className ="counteLikes">
-                             <p>5</p>
-                        </div>
-                   </div>
-
-
+        fetch('http://localhost:8000/social/', {method : 'GET'})
+         .then((response)=> response.json())
+         .then(responseJson=>{this.setState({data :responseJson})}
+          );
+     } 
+    
+    render(){
+        if (this.state.data.length > 0) {
+            return (
+                <div className ="social_app">
+    
+                <div className = "tittle">
+                    <div><h1>Dream Cakers</h1></div>
                 </div>
-
-
-                <div className ="customer">
-                   <div className ="cakeC">
-                       <img src ={imagen1} className ="cake"/>
-                   </div>
-                   <div className ="photoC">
-                      <img src ={imagen2} className ="photo"/>
-                   </div>
-
-                    <div className="infoC">
-                        <div className ="nameC">
-                             <p>Andres Flipe Snchez Cuatindioy</p>
-                        </div>
-                        <div className ="date">
-                            <p>6/marzo/2021</p>
-                        </div>
-                        <div className ="counteLikes">
-                             <p>5</p>
-                        </div>
-                   </div>
-
-
+                <div className ="content">
+                    <div>
+                        {this.state.data.map(e=>this.usuarios(e))} 
+                    </div>
                 </div>
+                
+            </div>
+            )
+            } else {
+                  return <p className="text-center">Cargando empleados...</p>
+            }
+        }
 
-
-                <div className ="customer">
-                   <div className ="cakeC">
-                       <img src ={imagen1} className ="cake"/>
-                   </div>
-                   <div className ="photoC">
-                      <img src ={imagen2} className ="photo"/>
-                   </div>
-
-                    <div className="infoC">
-                        <div className ="nameC">
-                             <p>Andres Flipe Snchez Cuatindioy</p>
-                        </div>
-                        <div className ="date">
-                            <p>6/marzo/2021</p>
-                        </div>
-                        <div className ="counteLikes">
-                             <p>5</p>
-                        </div>
-                   </div>
-
-
-                </div>
-
-           </div>
-       </div>
-       
-   </div>
-  );
 }
-
 export default Social_app;
