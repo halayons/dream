628c54549d9cbdd63aa2cdda3204a9840bb5cca3
cdsaineac <cdsaineac@unal.edu.co>
2021-06-26 20:15:59 -0500

    Boton de Moderacion funcional Ver/Ocultar

--- src/components/Moderacion/moderar.jsx
+++ src/components/Moderacion/moderar.jsx
@@ -6,7 +6,7 @@ export class Moderar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-          status:true
+          status: !this.props.datos.status
         };
 
         this.onSubmitForm = this.onSubmitForm.bind(this);
@@ -16,17 +16,17 @@ export class Moderar extends React.Component {
         const requestOptions = {
           method: 'PUT',
           headers: {
-            // 'Content-Type': 'application/json',
+            'Content-Type': 'application/json',
             'X-CSRFToken': Cookies.get('csrftoken')
           },
           credentials: "include",
-          
+          body:JSON.stringify(this.state)
         };
         console.log(requestOptions)
         fetch('http://localhost:8000/social/mod_post/' + this.props.datos.id, requestOptions)
           .then(response => response.json())
           .then(json => this.setState({
-            posts: json
+            status: !this.state.status
           }))
           .catch(error => console.log(error));
       }
